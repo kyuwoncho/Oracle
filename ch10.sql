993c2b441bf077967d352ee47444f39d0fa94a41
kyuwoncho <81146049+kyuwoncho@users.noreply.github.com>
2022-06-03 14:00:11 +0900

    Add files via upload

--- ch10.sql
+++ ch10.sql
@@ -1,36 +1,50 @@
---테이블 생성과 관리
---1. 회원 정보를 저장하는 테이블(member)을 생성하시오.
--- 회원정보는 사용자아이디(15), 이름(20), 비밀번호(20), 전화번호(15), 이메일(100)을 포함
+--ch10
+-- 1. 회원 정보를 저장하는 테이블(member)을 생성하세요.
+-- 회원정보는 사용자아이디(15), 이름(20), 비밀번호(20), 전화번호(15), 이메일(100)을 포함해야한다.
 create table member(
-    userid VARCHAR2(15) not null,
-    name VARCHAR2(20) not null,
-    password VARCHAR2(20) not null,
-    phone varchar2(15),
-    email varchar2(100)
-);
+    userid VARCHAR2(15) NOT NULL,
+    name VARCHAR2(20) NOT NULL,
+    password VARCHAR2(20) NOT NULL,
+    phone VARCHAR2(15),
+    email VARCHAR2(100)
+    );
+    
+select *
+from member;
 
---2. 사용자아이디, 이름, 비밀번호, 전화번호, 이메일을 저장하는 쿼리를 적성하기.
---user123, 사용자, a1234567890, 011-234-5678, user@user.com
-insert into 
-    member(userid, name, password, phone, email)
-    values ('user123', '사용자', 'a1234567890', '011-234-5678','user@user.com');
+-- 2. 사용자아이디, 이름, 비밀번호, 전화번호, 이메일을 저장하는 쿼리를 작성하세요. 
+-- user123, 사용자, a1234567890, 011-234-5678, user@user.com
+insert into
+    member(userid,name,password,phone,email)
+    values('user123','사용자','a1234567890','011-234-5678','user@user.com');
 
---3. 사용자아이디가 user123인 사용자의 모든 정보를 조회하시오.
-select * from member
+select*
+from member;
+
+-- 3. 사용자아이디가 user123인 사용자의 모든 정보를 조회하세요.
+select *
+from member
 where userid='user123';
 
---4. 사용자 아이디가 user123인 사용자의 이름, 비밀번호, 전화번호, 이메일을 수정하세요.
---홍길동,a1234,011-222-3333,user@user.co.kr
+-- 4. 사용자아이디가 user123인 사용자의 이름, 비밀번호, 전화번호, 이메일을 수정하세요
+-- 홍길동,a1234,011-222-3333, user@user.co.kr
 update member
-set name='홍길동',password = 'a1234', phone='011-222-3333', email = 'user@user.co.kr'
+set name = '홍길동', password = 'a1234', phone ='011-222-333',email='user@user.co.kr'
 where userid = 'user123';
 
---5. 사용아이디 =user123, pw=a1234인 회원 정보 삭제
+select *
+from member
+where name = '홍길동';
+
+-- 5. 사용자아이디가 user123이고 비밀번호가 a1234인 회원의 정보를 삭제하세요.
 delete from member
-where userid = 'user123' and password = 'a1234';
+where userid='user123' and password='a1234';
 
---6. 회원 정보를 저장하는 테이블(member)의 모든 행을 삭제하세요.
-TRUNCATE TABLE member;
+select *
+from member;
 
---7. 회원 정보를 저장하는 테이블(member)을 삭제하세요.
+-- 6. 회원 정보를 저장하는 테이블(member)의 모든 행을 삭제하세요. TRUNCATE구문을 이용
+truncate table member;
+
+-- 7. 회원 정보를 저장하는 테이블(member)를 삭제하세요.
 drop table member;
